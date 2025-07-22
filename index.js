5df0dbffa5d87a62f141fcb3475650216a24e150
Darryl <tchoffopharell@gmail.com>
2025-07-22 20:03:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+//1st Player
+var randomNumber1 = Math.ceil(Math.random()*6);
+var a = document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+
+//2nd Player
+var randomNumber2 = Math.ceil(Math.random()*6);
+var b = document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+
+//Setting up a winner
+if(randomNumber1 > randomNumber2){
+    var n = document.querySelector('h1').textContent = "Player 1 Winner!";
+}else{
+    var n = document.querySelector('h1').textContent = "Player 2 Winner!";
+}
